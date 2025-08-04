609ee1950ab1d6fdb885abdb8054fe173cee1b41
luigi <virtual.employee@gmail.com>
2025-08-04 17:06:33 -0600

    Added wezterm config and windows

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require 'wezterm'
+local config = {}
+config.term = "xterm-256color" 
+config.font = wezterm.font 'JetBrains Mono'
+config.font_size= 14.5
+config.color_scheme = 'Black Metal (Venom) (base16)'
+
+config.keys = {
+  { key = '`', mods = 'ALT', action = wezterm.action.ShowLauncher },
+}
+
+return config
