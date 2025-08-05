d80718bc73e1ef4e2348a705c0456c871c4e9f6f
luigi <virtual.employee@gmail.com>
2025-08-04 22:41:57 -0600

    added padding to wezterm

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -9,11 +9,4 @@ config.keys = {
 	{ key = "`", mods = "ALT", action = wezterm.action.ShowLauncher },
 }
 
-config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
-}
-
 return config
