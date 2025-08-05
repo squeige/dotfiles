7b30908d165fed4f0256d5ce11e0a5298567b956
luigi <virtual.employee@gmail.com>
2025-08-04 22:36:03 -0600

    added some tmux and wezterm configs

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,12 +1,19 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = {}
-config.term = "xterm-256color" 
-config.font = wezterm.font 'JetBrains Mono'
-config.font_size= 14.5
-config.color_scheme = 'Black Metal (Venom) (base16)'
+config.term = "xterm-256color"
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 14.5
+config.color_scheme = "Black Metal (Venom) (base16)"
 
 config.keys = {
-  { key = '`', mods = 'ALT', action = wezterm.action.ShowLauncher },
+	{ key = "`", mods = "ALT", action = wezterm.action.ShowLauncher },
+}
+
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 
 return config
